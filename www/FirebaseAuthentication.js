16d6a47bf3c8178f53710766431d3d31aa90234b
Ng Beng Chet <bengchet@cytron.com.my>
2018-03-01 15:29:28 +0800

    Update FirebaseAuthentication.js

--- www/FirebaseAuthentication.js
+++ www/FirebaseAuthentication.js
@@ -1,4 +1,4 @@
-var exec = require("cordova/exec");
+/*var exec = require("cordova/exec");
 var PLUGIN_NAME = "FirebaseAuthentication";
 
 module.exports = {
@@ -21,5 +21,35 @@ module.exports = {
     },
     signOut: function() {
         exec(success, error, PLUGIN_NAME, "signOut", []);
+    }
+};*/
+var exec = require("cordova/exec");
+var PLUGIN_NAME = "FirebaseAuthentication";
+
+module.exports = {
+    getIdToken: function(forceRefresh) {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, PLUGIN_NAME, "getIdToken", [forceRefresh]);
+        });
+    },
+    signInWithEmailAndPassword: function(email, password) {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, PLUGIN_NAME, "signInWithEmailAndPassword", [email, password]);
+        });
+    },
+    verifyPhoneNumber: function(phoneNumber, timeout) {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, PLUGIN_NAME, "verifyPhoneNumber", [phoneNumber, timeout]);
+        });
+    },
+    signInWithVerificationId: function(verificationId, code) {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, PLUGIN_NAME, "signInWithVerificationId", [verificationId, code]);
+        });
+    },
+    signOut: function() {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, PLUGIN_NAME, "signOut", []);
+        });
     }
 };
